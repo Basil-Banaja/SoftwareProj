43d5efe1057084c78760535e22d0b7d4133b602a
BasilBanaja <basil.banaja@gmail.com>
2023-12-19 19:24:58 +0300

    Final Edit

--- src/Main.java
+++ src/Main.java
@@ -12,38 +12,59 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         UserAccount userAccount = new UserAccount();
-        userAccount.createAccount();
+        Scanner scanner = new Scanner(System.in);
 
-        if (userAccount.login()) {
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                System.out.println("Menu:");
-                System.out.println("1. Show Driver History");
-                System.out.println("2. Exit");
-                System.out.print("Enter your choice (1 or 2): ");
-                int choice = scanner.nextInt();
+        while (true) {
+            System.out.println("Welcome to the PmuRide App");
+            System.out.println("1. Create a New Account");
+            System.out.println("2. Login to Existing Account");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice (1, 2, or 3): ");
+            int choice = scanner.nextInt();
 
-                if (choice == 1) {
-                    // Creating an array of driver histories
-                    DriverHistory[] histories = {
-                        new DriverHistory("Ahmed", 150.0, 2.5),
-                        new DriverHistory("Fatima", 60.0, 1.0),
-                        new DriverHistory("Youssef", 85.3, 1.75),
-                        new DriverHistory("Aisha", 45.7, 0.8),
-                        new DriverHistory("Khalid", 120.0, 2.0)
-                    };
-
-                    // Displaying each driver's history
-                    System.out.println("Driver Histories:");
-                    for (DriverHistory history : histories) {
-                        System.out.println(history);
-                    }
-                } else if (choice == 2) {
-                    System.out.println("Exiting the application.");
+            switch (choice) {
+                case 1:
+                    userAccount.createAccount();
                     break;
-                } else {
-                    System.out.println("Invalid choice. Please enter 1 or 2.");
-                }
+                case 2:
+                    if (userAccount.login()) {
+                        boolean loggedIn = true;
+                        while (loggedIn) {
+                            System.out.println("1. View Driver History");
+                            System.out.println("2. Exit");
+                            System.out.print("Enter your choice (1 or 2): ");
+                            int postLoginChoice = scanner.nextInt();
+
+                            switch (postLoginChoice) {
+                                case 1:
+                                    DriverHistory[] histories = {
+                                        new DriverHistory("Ahmed", 150.0, 2.5),
+                                        new DriverHistory("Fatima", 60.0, 1.0),
+                                        new DriverHistory("Youssef", 85.3, 1.75),
+                                        new DriverHistory("Aisha", 45.7, 0.8),
+                                        new DriverHistory("Khalid", 120.0, 2.0)
+                                    };
+
+                                    System.out.println("Driver Histories:");
+                                    for (DriverHistory history : histories) {
+                                        System.out.println(history);
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("Logging out...");
+                                    loggedIn = false;
+                                    break;
+                                default:
+                                    System.out.println("Invalid choice. Please enter 1 or 2.");
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Thank you for using the PmuRide App.");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
             }
         }
     }
